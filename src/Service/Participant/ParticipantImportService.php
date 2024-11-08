c1666c81a91d1de9f97a306d004f45796dd74d1c
Leo Texier <leo.texier2024@campus-eni.fr>
2024-11-08 16:41:27 +0100

    csv import

--- src/Service/Participant/ParticipantImportService.php
+++ src/Service/Participant/ParticipantImportService.php
@@ -2,6 +2,7 @@
 
 namespace App\Service\Participant;
 
+use App\Entity\Campus;
 use App\Entity\Participant;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -34,21 +35,39 @@ class ParticipantImportService
     private function createUserFromData(array $data): void
     {
         $email = $data[0];
-        $firstName = $data[1];
-        $lastName = $data[2];
-        $password = $data[3];
-        $roles = explode(',', $data[4]);
+        $roles = explode(',', $data[1]);
+        $password = $data[2];
+        $lastName = $data[3];
+        $firstName = $data[4];
+        $pseudo = $data[9];
+        $phoneNumber = $data[5];
+        $campusId = $data[7]; // ID du campus
 
+        // Récupérer le campus via son repository
+        $campus = $this->entityManager->getRepository(Campus::class)->find(53);
+
+        // Vérifier si le campus existe, sinon gérer l'erreur
+        if (!$campus) {
+            throw new \Exception('Campus non trouvé pour l\'ID ' . $campusId);
+        }
+
+        // Créer un nouvel utilisateur
         $user = new Participant();
         $user->setEmail($email);
         $user->setFirstName($firstName);
         $user->setLastName($lastName);
         $user->setRoles($roles);
-        $user->setIsActive(true);
+        $user->setActive(true);
+        $user->setPseudo($pseudo);
+        $user->setPhoneNumber($phoneNumber);
+        $user->setCampus($campus);
+        $user->setBrochureFilename(null); // Laisser null pour un champ de fichier
 
+        // Hacher le mot de passe
         $hashedPassword = $this->passwordHasher->hashPassword($user, $password);
         $user->setPassword($hashedPassword);
 
+        // Persister l'utilisateur en base de données
         $this->entityManager->persist($user);
         $this->entityManager->flush();
     }
