f31af8ba6171d4f6d6007defd63cd70982871e95
kimptgr <163475107+kimptgr@users.noreply.github.com>
2024-11-16 16:36:17 +0100

    Update install_env.sh
    change symfony console int php bin/console

--- presentationFinale/install_env.sh
+++ presentationFinale/install_env.sh
@@ -18,16 +18,16 @@ echo "Tailwind CSS est configuré"
 
 # Suppression de la base de données
 echo "Suppression de la base de données si elle existe..."
-symfony console doctrine:database:drop --force
+php bin/console doctrine:database:drop --force
 
 # Création de la base de données
 echo "Création de la base de données..."
-symfony console doctrine:database:create
+php bin/console doctrine:database:create
 
 # Génération et exécution des migrations
 echo "Exécution des migrations..."
-symfony console make:migration
-symfony console doctrine:migrations:migrate --no-interaction
+php bin/console make:migration
+php bin/console doctrine:migrations:migrate --no-interaction
 echo "Base de données créée et migrations appliquées"
 
 # Chargement des fixtures
@@ -37,12 +37,10 @@ echo "Fixtures chargées"
 
 # Nettoyage et compilation du cache et des assets
 echo "Nettoyage du cache et compilation des assets..."
-symfony console cache:clear
-symfony console asset:install
-symfony console asset:compile
+php bin/console asset:install
+php bin/console asset:compile
 
 echo "Passage en environnement de production (prod)"
 sed -i 's/^APP_ENV=.*/APP_ENV=prod/' .env
 
-echo "Initialisation terminée avec succès. Bravo !"
 echo "Initialisation terminée avec succès. Bravo !"
